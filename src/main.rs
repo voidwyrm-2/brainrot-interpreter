a13b65735b6d4d70ad6c678464af53a2b3608dfd
Radioactivity <85001747+voidwyrm-2@users.noreply.github.com>
2024-05-13 20:38:02 -0400

    The interpreter is now completely finished, it can't read from files but I don't know I'm gonna add that

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::env;
+use std::io::stdin;
+use std::{collections::HashMap, env};
 
 fn main() {
     //println!("WHAT");
@@ -35,9 +36,42 @@ fn run_bf(bfcode_raw: String) {
         bfcode_iter.push(c)
     }
 
+    // pre-interpretation
+    let mut jumps: HashMap<u32, u32> = HashMap::new();
+    let mut jumpstack: Vec<u32> = Vec::new();
+    let mut ln: u32 = 0;
+    for (i, c) in bfcode_iter.iter().enumerate() {
+        if *c == '[' {
+            jumpstack.push(i as u32)
+        } else if *c == ']' {
+            if jumpstack.len() > 0 {
+                let opening: u32;
+                match jumpstack.pop() {
+                    Some(i_32) => opening = i_32,
+                    None => {
+                        println!("got None when expecting Some from 'opening'");
+                        return;
+                    }
+                }
+                jumps.insert(opening, i as u32);
+                jumps.insert(i as u32, opening);
+            } else {
+                println!("error: unmatched ']' on line {}, col {}", ln + 1, i + 1);
+                return;
+            }
+        } else if *c == '\n' {
+            ln += 1;
+        }
+    }
+
+    if jumpstack.len() > 0 {
+        println!("error: unmatched '['");
+        return;
+    }
+
     let mut idx = 0;
 
-    let mut bytes: [i8; 30000] = [0; 30000];
+    let mut bytes: [u8; 30000] = [0; 30000];
     let mut pointer: usize = 0;
 
     while idx < bfcode_iter.len() {
@@ -82,6 +116,70 @@ fn run_bf(bfcode_raw: String) {
             '.' => {
                 println!("i{}({})", pointer, bytes[pointer]);
                 idx += 1;
+            }
+
+            '[' => {
+                //println!("{:?}", jumps);
+                if bytes[pointer] == 0 {
+                    match jumps.get(&(idx as u32)) {
+                        Some(out) => idx = 1 + (*out as usize),
+                        None => {
+                            println!(
+                                "error at '[': tried to get value {}, but it didn't exist",
+                                idx
+                            );
+                            return;
+                        }
+                    }
+                } else {
+                    idx += 1
+                }
+            }
+
+            ']' => {
+                //println!("{:?}", jumps);
+                if bytes[pointer] != 0 {
+                    match jumps.get(&(idx as u32)) {
+                        Some(out) => idx = 1 + (*out as usize),
+                        None => {
+                            println!(
+                                "error at ']': tried to get value {}, but it didn't exist",
+                                idx
+                            );
+                            return;
+                        }
+                    }
+                } else {
+                    idx += 1
+                }
+            }
+
+            ',' => {
+                loop {
+                    println!("please input a number in the range of 0-127, inclusive");
+
+                    let mut buffer = String::new();
+
+                    // `read_line` returns `Result` of bytes read
+                    let _ = stdin().read_line(&mut buffer);
+                    let given = buffer.trim_end().to_string();
+                    let conversion_res = given.parse::<i16>();
+                    match conversion_res {
+                        Ok(o) => {
+                            if o < 0 {
+                                println!("that's outside of the valid range!");
+                                continue;
+                            }
+                            bytes[pointer] = o as u8;
+                            idx += 1;
+                            break;
+                        }
+                        Err(e) => {
+                            println!("that's not a number!(err: {})", e);
+                            continue;
+                        }
+                    }
+                }
             }
 
             _other => {
